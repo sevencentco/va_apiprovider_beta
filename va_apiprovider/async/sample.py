bfe27651adf20ebeb3cc4bda6236014b75adf76a
VietAnh <nva.clsp@gmail.com>
2026-01-12 16:40:54 +0700

    [update] fix bug Developer Experience

--- va_apiprovider/async/sample.py
+++ va_apiprovider/async/sample.py
@@ -1,9 +1,23 @@
+# SQLALCHEMY_DATABASE_URI = "postgresql+asyncpg://user:pass@localhost/mydb"
 
-@app.delete("/users/<user_id:int>")
-async def mandas(request, user_id):
-    user = await db.session.get(User, user_id)
-    if not user:
-        return json({"ok": False}, status=404)
+# @app.delete("/users/<user_id:int>")
+# async def mandas(request, user_id):
+#     user = await db.session.get(User, user_id)
+#     if not user:
+#         return json({"ok": False}, status=404)
 
-    await db.session.delete(user)
-    return json({"ok": True})
+#     await db.session.delete(user)
+#     return json({"ok": True})
+
+
+# ###
+# await session.get(Model, id)
+# await session.execute(stmt)
+# await session.scalars(stmt)
+# await session.scalar(stmt)
+
+# await session.flush()
+# await session.commit()
+# await session.rollback()
+# await session.refresh(obj)
+# await session.close()
